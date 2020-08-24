d9f8431892230540ade58b312f1a5ef4f4882cba
Kartik Agarwal <kartikagarwal.1993@gmail.com>
2020-08-25 00:35:48 +0530

    Updates

---
+++ precache-manifest.69f8f2779e8f2a7394b31f6053ba685e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428b1b586e042106801584046166279a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7ac3a2235c7d6829957",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a1a9f2c73d0a9fc3b20",
+    "url": "/static/js/2.51b4f8f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.51b4f8f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ac3a2235c7d6829957",
+    "url": "/static/js/main.0a604567.chunk.js"
+  },
+  {
+    "revision": "68ac6399a4a2e4a34aaa",
+    "url": "/static/js/runtime-main.c3f34189.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
